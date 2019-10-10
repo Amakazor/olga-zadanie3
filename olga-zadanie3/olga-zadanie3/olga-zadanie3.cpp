d59702c6365022f02f7831fe447b06a2af08a0d3
Olga Krzyczkowska <krzyczkowskaolga@outlook.com>
2019-10-11 00:50:51 +0200

    Zatwierdź . . . . . . Albo będzie mi przykro

--- olga-zadanie3/olga-zadanie3/olga-zadanie3.cpp
+++ olga-zadanie3/olga-zadanie3/olga-zadanie3.cpp
@@ -17,20 +17,20 @@ int main()
 
 			switch (test) // w nawiasach zamiast wyra¿enia logicznego zmienna liczbowa ca³kowita (switch rozwa¿a wszystkie przypadki, nie tylko jeden konkretny)
 			{
-				case 1: // po case stawiasz wartoœæ dla której bêdzie siê wykonywaæ nastêpna czêœæ. Dwukropek sygnalizuje pocz¹tek bloku (zastêpuje nawiasy klamrowe)
-					std::cout << "Dla wartosci 1\n\n";
-					break; // break przerywa wykonywanie switch. Jeœli go nie zrobisz wykona siê tak¿e kolejny case. I kolejne tak d³ugo a¿ nie napotka break. Z regu³y jest to nieporz¹dane
-				case 13:
-					std::cout << "Dla wartosci 13. Ta czesc wykonanie sie domyslnie z moim ustawieniem zmiennej test\n\n";
-					break;
-				case 15:
-					std::cout << "Dla wartosci 15\n\n";
-				case 16:
-					std::cout << "Dla wartosci 16, ale wykona sie takze dla 15 przez to, ze w poprzednim case nie bylo break\n\n";
-					break;
-				default:
-					std::cout << "Dla kazdej innej wartosci niz wymienione w case\n\n";
-					break;
+			case 1: // po case stawiasz wartoœæ dla której bêdzie siê wykonywaæ nastêpna czêœæ. Dwukropek sygnalizuje pocz¹tek bloku (zastêpuje nawiasy klamrowe)
+				std::cout << "Dla wartosci 1\n\n";
+				break; // break przerywa wykonywanie switch. Jeœli go nie zrobisz wykona siê tak¿e kolejny case. I kolejne tak d³ugo a¿ nie napotka break. Z regu³y jest to nieporz¹dane
+			case 13:
+				std::cout << "Dla wartosci 13. Ta czesc wykonanie sie domyslnie z moim ustawieniem zmiennej test\n\n";
+				break;
+			case 15:
+				std::cout << "Dla wartosci 15\n\n";
+			case 16:
+				std::cout << "Dla wartosci 16, ale wykona sie takze dla 15 przez to, ze w poprzednim case nie bylo break\n\n";
+				break;
+			default:
+				std::cout << "Dla kazdej innej wartosci niz wymienione w case\n\n";
+				break;
 			}
 		}
 		/*KONIEC PRZYK£AD 3.1*/
@@ -76,7 +76,7 @@ int main()
 
 		/*ZADANIE 3.1*/
 		// Przerób Twój kod z poprzedniego zadania. W miejscach gdzie to stosowne wykorzystaj switch zamiast if, else if, else
-		if(!switch_current_exercise) //w nawiasie przed zmienn¹ jest negacja logiczna. Spowoduje to, ¿e zmienna zostanie odwrócona. Dla false wypali if, a dla true else
+		if (!switch_current_exercise) //w nawiasie przed zmienn¹ jest negacja logiczna. Spowoduje to, ¿e zmienna zostanie odwrócona. Dla false wypali if, a dla true else
 		{
 			int liczba_jeden = 0;
 			int liczba_dwa = 0;
@@ -164,12 +164,12 @@ int main()
 		}
 		/*KONIEC ZADANIE 3.1*/
 		else
-		/*ZADANIE 3.2*/
-		//Napisz program na obliczanie danej potegi liczby.
-		//1. pobierz z konsoli podstawê oraz wyk³adnik.
-		//2. opieraj¹c siê o wartoœæ wyk³adnika prze³¹cz ilokrotnie zmienna bêdzie mno¿ona przez siebie wykorzystuj¹c switch.
-		//3. oblicz wartoœæ (¿eby nie by³o za du¿o roboty to dla wyk³adników 0 1 2 3 4).
-		//4. wypisz wynik na konsolê.
+			/*ZADANIE 3.2*/
+			//Napisz program na obliczanie danej potegi liczby.
+			//1. pobierz z konsoli podstawê oraz wyk³adnik.
+			//2. opieraj¹c siê o wartoœæ wyk³adnika prze³¹cz ilokrotnie zmienna bêdzie mno¿ona przez siebie wykorzystuj¹c switch.
+			//3. oblicz wartoœæ (¿eby nie by³o za du¿o roboty to dla wyk³adników 0 1 2 3 4).
+			//4. wypisz wynik na konsolê.
 		{
 
 		}
